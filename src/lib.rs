7a5f93e5951c9dfbf89eac80524eac9967b33111
Andreas Widen <aw@luflow.net>
2026-08-14 14:01:27 +0200

    docs: added some app info and example usage
    Signed-off-by: Andreas Widen <aw@luflow.net>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,69 @@
 // flow-texpack: A program that will allow you to generate texture atlas.
 // zlib License (see LICENSE)
 
+//! `flow-texpack` is a program that will allow you to generate texture atlas from input images (BMP,
+//! HDR, JPG, PNG, TGA, TIFF, WEBP). The application generates both texture atlas and descriptions
+//! file that can be read by a game.
+//!
+//! ## Usage
+//! Show available options:
+//! ```sh
+//! flow-texpack -h
+//! ```
+//!
+//! or
+//!
+//! ```sh
+//! flow-texpack --help
+//! ```
+//!
+//! ## Examples
+//!
+//! Generate from input `data/characters` and `data/tiles`, write output to `out/atlas` and enable
+//! the options: `premultiply` pixels by their alpha channel, `trim` excess transparency off the
+//! textures, `remove duplicate textures` from the atlas, enable `rotation` of textures 90 degrees
+//! clockwise, `pad` each texture by 2 pixels and finally enable `verbose` output mode.
+//!
+//! ```sh
+//! flow-texpack -i data/characters data/tiles -o out/atlas -m -t -u -r -p 2 -v
+//! ```
+//!
+//! Enable `load filter` so that only `TGA` images are included in the texture atlas:
+//!
+//! ```sh
+//! flow-texpack -i data/tiles -o out/atlas --load-filter tga -v
+//! ```
+//!
+//! Enable rect heuristic `AreaFit`:
+//!
+//! ```sh
+//! flow-texpack -i data/tiles -o out/atlas --rect-heuristic area-fit -v
+//! ```
+//!
+//! Enable output `atlas size` of **2048x2048**:
+//!
+//! ```sh
+//! flow-texpack -i data/tiles -o out/atlas --atlas-size pot2048 -v
+//! ```
+//!
+//! Read input files/directories from `input.txt` but exclude all in `exclude.txt`:
+//!
+//! ```sh
+//! flow-texpack --input-file input.txt --exlude-file exclude.txt -o out/atlas -v
+//! ```
+//!
+//! `Adjust atlas size` automatically so that texture will fit:
+//!
+//! ```sh
+//! flow-texpack -i data/characters -o out/atlas --adjust-size -v
+//! ```
+//!
+//! `Adjust texture size` so that it will fit given atlas size:
+//!
+//! ```sh
+//! flow-texpack -i data/characters -o out/atlas --adjust-fit -v
+//! ```
+
 #[doc(hidden)]
 pub mod texpack;
 
